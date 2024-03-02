daf6c609db8de849fbf28e6dd940c0d820c4df59
Pablo Souza <pablo.osouza@outlook.com>
2024-03-02 08:04:05 -0300

    test: deletion

---
+++ test/CleanSheet.UnitTests/Entities/EntityTests.cs
@@ -0,0 +1,19 @@
+﻿namespace CleanSheet.UnitTests.Entities;
+public class EntityTests
+{
+    [Fact]
+    [DisplayName("Entity should change IsDeleted to true when Delete")]
+    public void Entity_Should_ChangeIsDeletedToTrue_When_Delete()
+    {
+        // Arrange
+        var manager = new Career(Guid.NewGuid(), "Testing");
+        var isDeleted = manager.IsDeleted;
+
+        // Act
+        manager.Delete();
+
+        // Assert
+        manager.IsDeleted.Should().BeTrue();    
+        manager.IsDeleted.Should().NotBe(isDeleted);
+    }
+}
---
+++ test/CleanSheet.UnitTests/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using CleanSheet.Domain.Entities;
+global using FluentAssertions;
+global using System.ComponentModel;
+global using Xunit;
